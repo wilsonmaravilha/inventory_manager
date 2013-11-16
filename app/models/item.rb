6beae99e5d6bd82a175643b78695f30755c9d19b
Wilson Maravilha <wilsonmaravilha@gmail.com>
2013-11-16 21:27:47 +0200

    Added default values for "new item" creation form

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,6 @@ class Item < ActiveRecord::Base
 	validates :qty_current, presence: true
 	validates :qty_ideal, presence: true
 	validates :exp_date, presence: true
+	validates :qty_sold, presence: true
 	belongs_to :user
 end
